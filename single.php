d4f60f80cbb79b99a54a1055e81d4d09df80f03f
kramskoy-v <kramskoy.va@gmail.com>
2024-02-17 13:48:21 +0300

    Правка стилей и single.php

--- single.php
+++ single.php
@@ -1,17 +1,27 @@
 <?php get_header(); ?>
 
 <main class="single">
-	<div class="content">
-		<?php
-		while (have_posts()) :
-			the_post(); ?>
-			<h1 class="page-title"><?php the_title() ?></h1>
-			<a class="btn ml" href="<?php echo get_post_type_archive_link('quiz') ?>" rel="noopener noreferrer">← назад</a>
-			<?php the_content();  ?>
-	</div>
-<?php endwhile;
-?>
-</div>
+    <div class="content">
+        <?php
+        while (have_posts()) :
+            the_post(); ?>
+            <h1 class="page-title"><?php the_title() ?></h1>
+            <?php if (is_singular('methodical')) {
+            the_content(); ?>
+            <a class="btn ml" href="<?php the_field('cloud_link') ?>/" target="_blank" rel="noopener noreferrer">
+                Перейти и скачать
+            </a>
+            <a class="btn" href="<?php echo get_post_type_archive_link('methodical') ?>" rel="noopener noreferrer">
+                ← назад
+            </a>
+        <?php } else { ?>
+            <a class="btn ml" href="<?php echo get_post_type_archive_link('quiz') ?>" rel="noopener noreferrer">
+                ← назад</a>
+            <?php the_content();
+        } ?>
+        <?php endwhile;
+        ?>
+    </div>
 </main>
 
 <?php get_footer() ?>;
